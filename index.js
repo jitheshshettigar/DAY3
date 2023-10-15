646e63192adc6dc12272fd00d4a0584e8a470af5
jitheshshettigar <129665611+jitheshshettigar@users.noreply.github.com>
2023-10-15 23:25:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+//prompt("Enter your name","");
+//console.log("This is console");
+//console.log(6+5);
+//document.write("<h1>Javascript</h1>");
+
+//var num1=5;
+//var num1=6;
+//num1=8
+//console.log(num1);
+
+//let num2=9
+// let num2= 10
+//num2=11
+//console.log(num2);
+
+//const num3=10
+// const num3=11// we can't redeclare the variable
+//num3=15// we can't reassign the variable
+//console.log(num3);
+
+// let num1=5
+// let num2="5"
+// console.log(num1==num2);
+// console.log(num1===num2);
+
+// function mul(n1, n2, n3, n4) {
+//     return n1*n2*n3*n4;
+// }
+// document.write(mul(4,5,6,7));
+// function greet() {
+//     alert("Hello Everyone");
+// }
+
+// let age = 10;
+// if (age  >= 18){
+//     document.write("Eligible for voye");
+// } else {
+//     document.write("not eligible");
+// }
+
+let ele = document.queryEslsctor("#head").innerHTML;
+console.log(ele);
